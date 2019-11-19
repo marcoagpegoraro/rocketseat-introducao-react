84ee269f6d28ef04df281448b099bced240a670b
Marco Antonio Gonçalves Pegoraro <magpegoraro@ibm.com>
2019-11-19 15:43:34 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+npm add @babel/core @babel/preset-env @babel/preset-react webpack webpack-cli -D  
+npm add react react-dom  
+npm add babel-loader -D  
+npm add webpack-dev-server -D  
---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    presets: [
+        '@babel/preset-env', // import export, arrow funciton, classes
+        '@babel/preset-react' // jsx e outras coisas do react
+    ]
+}
